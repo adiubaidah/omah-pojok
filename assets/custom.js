e8db511b23ec31d966bbca86a4181ef1ba2283b2
Adi12-lab <adiiskandarubaidah@gmail.com>
2024-08-28 20:26:10 +0700

    initial commit

---
+++ assets/custom.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  window.addEventListener("scroll", function () {
+    const navbar = $(".navbar");
+    if (window.scrollY > 100) {
+      navbar.addClass("fixed");
+    } else {
+      navbar.removeClass("fixed");
+    }
+  });
+
+  const filters = new Muuri(".items", {
+    items: ".item",
+  });
+
+  const buttonFilters = $(".filters button");
+  buttonFilters.each(function () {
+    $(this).on("click", function () {
+
+      buttonFilters.removeClass("active");
+      $(this).addClass("active");
+
+      const category = $(this).data("filter");
+      if (category === "semua") {
+        filters.filter("[data-category]");
+      } else {
+        filters.filter(`[data-category="${category}"]`);
+      }
+    });
+  });
+
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1, // Default value
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 1024px
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  })
+
+
+  
+});
